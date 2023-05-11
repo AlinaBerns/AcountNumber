51678f8516e056b02515c4cee28ddc38e3c75d8a
AlinaBerns <burns.alina@yandex.ru>
2023-05-11 15:21:32 +0200

    v

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Account {
+
+//Implementeer de UML
+public class Account {
+    private String accountNumber;
+
+    public Account(String accountNumber) {
+        setAccountNumber(accountNumber);
+    }
+
+    //public Account createNewAccount (String accountNumber) {
+        //return new Account(accountNumber);
+    //}
+    private void setAccountNumber(String accountNumber) {
+
+        //De formaat van het rekeningnummer moet zijn:
+        // 3 cijfers - 7 cijfers - 2 cijfers
+        if (accountNumber.matches("[0-9+]") && accountNumber.length() == 12) {
+            String regex = "\\d{3}-\\d{7}-\\d{2}";
+            this.accountNumber = (accountNumber).replaceAll(accountNumber, regex);
+        }
+
+
+        long num1 = Long.parseLong(accountNumber.substring(0,10));
+        long num2 = Long.parseLong(accountNumber.substring(11,12));
+
+        //Wanneer het formaat van het rekeningnummer niet juist
+        // is moet er een exception worden gegooid.
+        //throw new IllegalArgumentException("Not possible");
+
+        }
+
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                '}';
+    }
 }
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class App {
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.spi.DecimalFormatSymbolsProvider;
+
+public class App {
+    public static void main(String[] args) {
+        //int num = Integer.parseInt(String.valueOf(123));
+        //int num2 = Integer.parseInt(String.valueOf(4561238));
+        //int num3 = Integer.parseInt(String.valueOf(12));
+        //String accountNumber = (num + "-" + num2 + "-" + num3);
+
+
+        //System.out.println(accountNumber);
+
+        Account account = new Account("123156458712");
+        System.out.println(account);
+    }
 }
